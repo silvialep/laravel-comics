c82f5afdc8880a4f41082becc8ada5bb12c3457b
silvialep <silvia.lepore_sl@libero.it>
2023-05-09 17:44:02 +0200

    feat: added header + dynamic menu + limited style

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts/main-layout')
+
+
+@section('main-section')
+<div class="container">
+
+    <div class="row">
+        <div class="col-2">
+            
+        </div>
+
+    </div>
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts/main-layout');
+
+    $menulinks = config('menulinks');
+
+    $comics = config('comics');
+
+    // dd($menulinks);
+    // dd($comics);
+
+    return view('home', compact('menulinks', 'comics'));
 });
